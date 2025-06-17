e9cca28ecaaf0f30f831ddf3cb19141f6190fb27
Alejandro Garcia <garciaalejandro2552@gmail.com>
2025-06-17 04:41:18 -0500

    Main logic

---
+++ main.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agent import create_tool_calling_agent
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+
+load_dotenv()
+
+class ResearchResponse():
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+parser = PydanticOutputParser(pydantic_object="claude-3-5-sonnet-20241022")
+
+promt = ChatPromptTemplate.from_messages(
+[
+    (   
+        "system",
+        """
+        You are a research assistant that will help generate a research paper, 
+        Answer the user query and use necessary tools.
+        Wrap theoutput in this format and provide no other text\n{format_instructions}
+        """,
+    ),
+
+    ("placeholder", "{chat_history}"),
+    ("human", "{query}"),
+    ("placeholder", "{agent_scratchpad}")
+]
+).partial_instructions=parser.get_format_instructions()
+
+agent = create_tool_calling_agent(
+    llm = llm,
+    promt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
+print(raw_response)
+
+structured_response = parser.parse(raw_response.get("output")[0]["text"]) 
+print(structured_response)
+
